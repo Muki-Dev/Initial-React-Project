eb13b498560f92c8038e885cd2f789fcd664d9b4
Muki Dev <mukidev881@gmail.com>
2024-08-05 12:47:53 +0000

    add and modify code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
   render(){
     return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className='App-title'>Welcome to React</h1>
       </header>
+      <Page1  onRouteChange={onRouteChange} />
+      <Page2  onRouteChange={onRouteChange} />
+      <Page3  onRouteChange={onRouteChange} />
     </div>
   );
 }
