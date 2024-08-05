bfd5831cdd660fdf5e8853922384dc0c344d0990
Muki Dev <mukidev881@gmail.com>
2024-08-05 15:25:26 +0000

    Code Splitting part2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      route: 'page1'
+      route: 'page1',
+      component: ''
     }
   }
 
   onRouteChange = (route) => {
-    this.setState({route: route});
+    // No Code Splitting
+    // this.setState({route: route});
+
+    // With Code Splitting
+    if(route === 'page1'){
+      this.setState({route : route})
+    }else if(route === 'page2'){
+      import('./components/Page2').then((page2) => {
+        this.setState({route: route, component: page2.default})
+      })
+    }else if(route === 'page3'){
+      import('./components/Page3').then((page3) => {
+        this.setState({route: route, component: page3.default})
+      })
+    }
   }
 
   render(){
+    // No Code Splitting
     //   if(this.state.route === 'page1'){
     //    return <Page1  onRouteChange={this.onRouteChange} />
     //  }else if(this.state.route === 'page2'){
@@ -24,6 +40,13 @@ class App extends Component{
     // }else if(this.state.route === 'page3'){
     //   return <Page3  onRouteChange={this.onRouteChange} /> 
     // }
+
+    // With Code Splitting
+    if(this.state.route === 'page1'){
+      return <Page1 onRouteChange={this.onRouteChange} />
+    }else{
+      return <this.state.component onRouteChange={this.onRouteChange} />
+    }
   }
 }
 
